228f98a71f263e7625507b8fae762ce272c928d4
PetiDev <peteristvanh@gmail.com>
2022-09-02 14:11:16 +0200

    style: Title fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - html alapok',
-    title: 'html alapok',
+    titleTemplate: '%s - PetiDev',
+    title: 'Fronend alapok',
     htmlAttrs: {
       lang: 'hu'
     },
