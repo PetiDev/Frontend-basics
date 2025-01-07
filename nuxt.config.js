50baf97d9585048837ae30e4da54967e32cb09bb
PetiDev <54474482+PetiDev@users.noreply.github.com>
2025-01-07 21:29:39 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,30 +54,7 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      light: false,
-      themes: {
-        dark: {
-          primary: "#212121",
-          accent: "#ffffff",
-          secondary: "#ff4757",
-          info: "#ff6348",
-          warning: "#ffa502",
-          error: "#ff4757",
-          success: "#2ed573"
-        },
-        light: {
-          primary: "#f1f2f6",
-          accent: "#212121",
-          secondary: "#ff4757",
-          info: "#ff7f50",
-          warning: "#eccc68",
-          error: "#ff6b81",
-          success: "#7bed9f"
-        }
-      }
-    }
+    optionsPath: './vuetify.options.js'
   },
 
 // Build Configuration: https://go.nuxtjs.dev/config-build
